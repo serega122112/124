f744a1d170ce83c225d7f163693ba5b8e1159010
Whatsapp1231234 <91056671+Whatsapp1231234@users.noreply.github.com>
2024-05-03 22:16:26 +0300

    Update gc-config.js

--- plugins/gc-config.js
+++ plugins/gc-config.js
@@ -1,4 +1,3 @@
-
 const handler = async (m, {conn, args, usedPrefix, command}) => {
   const datas = global
   const idioma = datas.db.data.users[m.sender].language
@@ -6,26 +5,26 @@ const handler = async (m, {conn, args, usedPrefix, command}) => {
   const tradutor = _translate.plugins.gc_config
 
   const isClose = { // Switch Case Like :v
-    'открыть': 'not_announcement',
+    'open': 'not_announcement',
     'close': 'announcement',
-    'закрыть': 'not_announcement',
+    'abierto': 'not_announcement',
     'cerrado': 'announcement',
-    'abrir': 'not_announcement',
-    'cerrar': 'announcement',
+    'открыть': 'not_announcement',
+    'закрыть': 'announcement',
   }[(args[0] || '')];
   if (isClose === undefined) {
     throw `
 ${tradutor.texto1[0]}
 
 ${tradutor.texto1[1]}
-*┠┉↯ ${usedPrefix + command} abrir*
-*┠┉↯ ${usedPrefix + command} cerrar*
+*┠┉↯ ${usedPrefix + command} открыть*
+*┠┉↯ ${usedPrefix + command} закрыть*
 `.trim();
   }
   await conn.groupSettingUpdate(m.chat, isClose);
   {m.reply(`${tradutor.texto1[0]}`);}
 };
-handler.help = ['group открыть / закрыть', 'grupo abrir / cerrar'];
+handler.help = ['group open / close', 'grupo abrir / cerrar'];
 handler.tags = ['group'];
 handler.command = /^(группу|grupo)$/i;
 handler.admin = true;
