ff3da243df42bdc57bc4eed73bfd539e4e856c15
Whatsapp1231234 <91056671+Whatsapp1231234@users.noreply.github.com>
2024-05-03 23:28:48 +0300

    Update config-funciones.js

--- plugins/config-funciones.js
+++ plugins/config-funciones.js
@@ -175,7 +175,7 @@ ${tradutor.texto25[3]}
 
 ${tradutor.texto26[0]} | MODEJADIBOT
 ${tradutor.texto26[1]} ${usedPrefix + command} режим
-${tradutor.texto26[2]} (${usedPrefix}serbot / ${usedPrefix}jadibot). 
+${tradutor.texto26[2]} (${usedPrefix}serbot / ${usedPrefix}режим). 
 ${tradutor.texto26[3]}
 
 --------------------------------
