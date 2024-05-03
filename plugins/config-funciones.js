9aaaedf8fe3006bb6704a47174151cd799ec3491
Whatsapp1231234 <91056671+Whatsapp1231234@users.noreply.github.com>
2024-05-03 20:52:31 +0300

    Update config-funciones.js

--- plugins/config-funciones.js
+++ plugins/config-funciones.js
@@ -486,5 +486,5 @@ break;
   conn.sendMessage(m.chat, {text: `_*${tradutor.texto28[0]}*_\n\n*${tradutor.texto28[1]}* _${type}_ *fue* ${isEnable ? '_включить_' : '_desactivada_'} *${tradutor.texto28[2]}* ${isAll ? '_bot._' : isUser ? '' : '_chat._'}`}, {quoted: m});
   //conn.sendMessage(m.chat, {text: `▢ *Opción:* ${type}\n\n▢ *Estado:* ${isEnable ? 'Activado' : 'Desactivado'}\n\n▢ *Para* ${isAll ? 'este bot' : isUser ? '' : 'este chat'}`}, {quoted: m});
 };
-handler.command = /^((вкл|выкл)ючить|(tru|fals)e|(turn)?[01])$/i;
+handler.command = /^((вк|вык)ючить|(tru|fals)e|(turn)?[01])$/i;
 export default handler;
