507fc60c3802864a75191615c20209ed6c897d6e
Whatsapp1231234 <91056671+Whatsapp1231234@users.noreply.github.com>
2024-05-04 14:12:03 +0300

    рп

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -37,7 +37,7 @@ const handler = async (m, {conn, usedPrefix}) => {
   
   ${tradutor.texto1[13]}`.trim();
   const buttonMessage= {
-    'document': {url: `https://github.com/BrunoSobrino/TheMystic-Bot-MD`},
+    'document': {url: ``},
     'mimetype': `application/${document}`,
     'fileName': `${tradutor.texto2[0]}`,
     'fileLength': 99999999999999,
@@ -46,13 +46,13 @@ const handler = async (m, {conn, usedPrefix}) => {
       'forwardingScore': 200,
       'isForwarded': true,
       'externalAdReply': {
-        'mediaUrl': 'https://github.com/BrunoSobrino/TheMystic-Bot-MD',
+        'mediaUrl': '',
         'mediaType': 2,
         'previewType': 'pdf',
         'title': tradutor.texto2[1],
         'body': wm,
         'thumbnail': imagen1,
-        'sourceUrl': 'https://www.youtube.com/channel/UCSTDMKjbm-EmEovkygX-lCA'}},
+        'sourceUrl': ''}},
     'caption': text,
     'footer': wm,
     // 'buttons':[
@@ -63,5 +63,5 @@ const handler = async (m, {conn, usedPrefix}) => {
 };
 handler.help = ['owner', 'creator'];
 handler.tags = ['info'];
-handler.command = /^(owner|creator|creador|propietario)$/i;
+handler.command = /^(создатель|creator|creador|propietario)$/i;
 export default handler;
