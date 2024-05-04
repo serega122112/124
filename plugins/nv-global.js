4e6fafc972e2c5b8986a014d17a9f27f74f2c331
Whatsapp1231234 <91056671+Whatsapp1231234@users.noreply.github.com>
2024-05-04 21:07:42 +0300

    Update nv-global.js

--- plugins/nv-global.js
+++ plugins/nv-global.js
@@ -225,7 +225,7 @@ handler.all = async function(m, {conn}) {
   if (!chat.isBanned && m.text.match(/(давай знакомиться)/gi)) {
     if (!db.data.chats[m.chat].audios) return;
     if (!db.data.settings[this.user.jid].audios_bot && !m.isGroup) return;
-    const vn = './media/давай знакомиться.mp3';
+    const vn = './media/давай знакомитьсяс.mp3';
     mconn.conn.sendPresenceUpdate('recording', m.chat);
     mconn.conn.sendMessage(m.chat, {audio: {url: vn}, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
   }
