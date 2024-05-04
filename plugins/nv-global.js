56460f3afb085cba5f7401afd68cdef0f6b1355e
Whatsapp1231234 <91056671+Whatsapp1231234@users.noreply.github.com>
2024-05-04 17:23:58 +0300

    Update nv-global.js

--- plugins/nv-global.js
+++ plugins/nv-global.js
@@ -46,7 +46,7 @@ handler.all = async function(m, {conn}) {
     mconn.conn.sendMessage(m.chat, {audio: {url: vn}, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
   }
 
-  if (!chat.isBanned && m.text.match(/хочу)/gi)) {
+  if (!chat.isBanned && m.text.match(/(хочу)/gi)) {
     if (!db.data.chats[m.chat].audios) return;
     if (!db.data.settings[this.user.jid].audios_bot && !m.isGroup) return;
     const vn = './media/хочу.mp3';
